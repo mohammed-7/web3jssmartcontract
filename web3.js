2008173c0796542e230583f2ed63b177a4a07fc2
mohammed-7 <khanhuzaif@live.com>
2023-12-13 12:50:33 +0530

    first commit

---
+++ web3.js
@@ -0,0 +1,12 @@
+step1: npm init -y 
+step2: npm install --save web3 
+step3: node check version 
+step4: let Web3=require("web3")
+step5: Web3 >
+step6: let web3 = new Web3(new web3.provider.HttpProviders("Ganache http"));
+step7:web3.eth.getBalance("account number random from ganache")
+step8:create myfirstsmartcontract
+step9:create a smartcontract 
+
+let mycontract = new web3.eth.contract([ABI COPY, copy contract])
+now compile the smart contract and deployment
